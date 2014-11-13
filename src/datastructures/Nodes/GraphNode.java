ca8e4e8c27146b525de2dfd5018d49c223d6b035
Development <Development@Lenovo.uccs.edu>
2014-11-13 15:45:26 -0700

    Various datastructures that I've implemented, moving towards a libary approach with regards to ShortCircuit

---
+++ src/datastructures/Nodes/GraphNode.java
@@ -0,0 +1,40 @@
+package DataStructures.Nodes;
+
+
+public class GraphNode<T> {
+
+    private Comparable element;
+    private int index;
+    private float[] coords;
+
+    public GraphNode(int _index, Comparable newElement) {
+        index = _index;
+        element = newElement;
+        setCoordArray();
+    }
+
+    public GraphNode(Comparable newElement) {
+        index = -1;
+        element = newElement;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public Comparable getElement() {
+        return element;
+    }
+
+    private void setCoordArray() {
+        String[] strArr = ((String) element).split(",");
+        coords = new float[strArr.length];
+        for (int i = 0; i < strArr.length; i++) {
+            coords[i] = Float.parseFloat(strArr[i]);
+        }
+    }
+
+    public float[] getCoordArray() {
+        return coords;
+    }
+}
